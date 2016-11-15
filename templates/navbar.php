46516d95f1636731df103723de1ee9ec83e21740
Irtiza Delwar <Irtiza Delwar>
2016-11-15 10:17:47 -0500

    Added the the processor file with navbar

---
+++ templates/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Rift Analytics</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#"><span class="glyphicon glyphicon-home"></span> Home </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-signal"></span> Champion Statistics </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-tasks"></span> Top Lists </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-eye-open"></span> Live Match Predictor </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-road"></span> Rankings </a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <button class="button" onclick="newDoc()"><span class="glyphicon glyphicon-search"></span> </button> <input name="searchTxt" type="text" maxlength="512" id="searchTxt" placeholder="Name.." class="searchField"/></li>
+      </ul>
+    </div>
+  </div>
+</nav>
