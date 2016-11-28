72d4732bc3af29d671c718f7752fe5c1097a4c24
ttgil95 <ttgil95@vt.edu>
2016-11-27 21:34:24 -0500

    Added few more tests

--- unit_tests.py
+++ unit_tests.py
@@ -19,6 +19,11 @@ class MyTest(TestCase):
 		#print(dict)
 		self.assertTrue(b'103' in dict)
 		
+	def test_champion_rotation_not(self):
+		dict = processor.champion_rotation()
+		dict = dict.data
+		self.assertFalse(b'10000' in dict)
+		
 	def test_valid_api_response_false(self):
 		testResponse = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Merky12345?api_key=" + processor.apiKey
 		testResponse = requests.get(testResponse)
@@ -28,6 +33,17 @@ class MyTest(TestCase):
 		testResponse = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Merky?api_key=" + processor.apiKey
 		testResponse = requests.get(testResponse)
 		self.assertTrue(processor.valid_api_request(testResponse))
+		
+	def test_champion_info(self):
+		app = self.create_app()
+		tester = app.test_client()
+		response = tester.get('/champion_info', headers=[('X-Requested-With'), ('XMLHttpRequest')])
+		response = response.data
+		print('response')
+		
+	def test_recent_game(self):
+		recent = processor.recent_game("na", "G0ldfish")
+		print(recent)
 		
 if __name__ == '__main__':
 	unittest.main()
