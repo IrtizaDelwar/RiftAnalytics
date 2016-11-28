dce229aed5c357a665ec33a5762f72db80e1b63c
Irtiza Delwar <Irtiza Delwar>
2016-11-28 02:21:52 -0500

    got rid of project classes folder

--- Project classes/Item.py
+++
@@ -1,9 +0,0 @@
-class Item:
-	def __init__(self, itemID):
-		self.ID = itemID
-	
-	def setName(self, itemName):
-		self.name = itemName
-	
-	def setDescription(self, itemDes):
-		self.description = itemDes
